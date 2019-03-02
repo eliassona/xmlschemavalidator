f323410d345ac74a962e387ea87e0b4ef2dd7e85
Anders Eliasson <anders.eliasson@digitalroute.com>
2019-03-02 14:23:54 +0100

    refactoring

--- test/xmlschemavalidator/core_test.clj
+++ test/xmlschemavalidator/core_test.clj
@@ -11,10 +11,10 @@
 		        <enumeration value=\"medium\"/>
 		        <enumeration value=\"large\"/>
 		      </restriction>")]
-	  (is (= [true "small"] (f "small" predef-types nil nil)))
-	  (is (= [true "medium"] (f "medium" predef-types nil nil)))
-	  (is (= [true "large"] (f "large" predef-types nil nil)))
-	  (is (= [false "asdf"] (f "asdf" predef-types nil nil)))
+	  (is (= [true "small"] (f "small" predef-types {} {})))
+	  (is (= [true "medium"] (f "medium" predef-types {} {})))
+	  (is (= [true "large"] (f "large" predef-types {} {})))
+	  (is (= [false "asdf"] (f "asdf" predef-types {} {})))
   ))
 
 (deftest test-range-restriction
@@ -22,9 +22,9 @@
 		        <minInclusive value=\"36\"/>
 		        <maxInclusive value=\"42\"/>
 		      </restriction>")]
-	  (is (= [true 36] (f 36 predef-types nil nil)))
-	  (is (= [true 42] (f 42 predef-types nil nil)))
-	  (is (= [false 43] (f 43 predef-types nil nil)))
+	  (is (= [true 36] (f 36 predef-types {} {})))
+	  (is (= [true 42] (f 42 predef-types {} {})))
+	  (is (= [false 43] (f 43 predef-types {} {})))
   ))
 
 
@@ -37,8 +37,8 @@
 		          </restriction>
              </simpleType>")]
     
-    (is (= [true 36] (f 36 predef-types nil nil)))
-    (is (= [false 43] (f 43 predef-types nil nil)))))
+    (is (= [true 36] (f 36 predef-types {} {})))
+    (is (= [false 43] (f 43 predef-types {} {})))))
 
 
 (deftest test-simple-type-def
@@ -50,8 +50,8 @@
 		          </restriction>
              </simpleType>")]
     (is (= :type (-> e meta :kind)))
-    (is (= [false 0] ((eval (first (vals e))) 0 predef-types nil nil)))
-    (is (= [true 36] ((eval (first (vals e))) 36 predef-types nil nil)))
+    (is (= [false 0] ((eval (first (vals e))) 0 predef-types {} {})))
+    (is (= [true 36] ((eval (first (vals e))) 36 predef-types {} {})))
     ))
 
 (deftest test-simple-type-reffing-predef
@@ -59,9 +59,9 @@
             "<simpleType name=\"aname\" type=\"byte\">
              </simpleType>")]
     (is (= :type (-> e meta :kind)))
-    (is (= [true 0] ((eval (first (vals e))) 0 predef-types nil nil)))
-    (is (= [true 127] ((eval (first (vals e))) 127 predef-types nil nil)))
-    (is (= [false 128] ((eval (first (vals e))) 128 predef-types nil nil)))
+    (is (= [true 0] ((eval (first (vals e))) 0 predef-types {} {})))
+    (is (= [true 127] ((eval (first (vals e))) 127 predef-types {} {})))
+    (is (= [false 128] ((eval (first (vals e))) 128 predef-types {} {})))
     
     ))
 
@@ -69,9 +69,9 @@
   (let [text "<element name=\"abyte\" type=\"byte\"/>"
         f (validation-fn-of text)]
     (is (= :element (-> (validation-expr-of text) meta :kind)))
-    (is (= [true 0 :abyte] ((f :abyte) 0 predef-types nil nil)))
-    (is (= [true 127 :abyte] ((f :abyte) 127 predef-types nil nil)))
-    (is (= [false 128 :abyte] ((f :abyte) 128 predef-types nil nil)))
+    (is (= [true 0 :abyte] ((f :abyte) 0 predef-types {} {})))
+    (is (= [true 127 :abyte] ((f :abyte) 127 predef-types {} {})))
+    (is (= [false 128 :abyte] ((f :abyte) 128 predef-types {} {})))
   ))
 
 
@@ -83,9 +83,9 @@
               <element name=\"abyte\" type=\"byte\"/>
               <element name=\"anint\" type=\"integer\"/>
              </schema>")]
-    (is (= [true 0 :anint] (f (parse-str "<anint>0</anint>") predef-types nil nil)))
-    (is (= [true 0 :abyte] (f (parse-str "<abyte>0</abyte>") predef-types nil nil)))
-    (is (= [false 128 :abyte] (f (parse-str "<abyte>128</abyte>") predef-types nil nil)))
+    (is (= [true 0 :anint] (f (parse-str "<anint>0</anint>") predef-types {} {})))
+    (is (= [true 0 :abyte] (f (parse-str "<abyte>0</abyte>") predef-types {} {})))
+    (is (= [false 128 :abyte] (f (parse-str "<abyte>128</abyte>") predef-types {} {})))
   ))
 
 (deftest test-schema-with-simple-type
@@ -101,11 +101,11 @@
               <element name=\"anint\" type=\"integer\"/>
               <element name=\"my\" type=\"mytype\"/>
              </schema>")]
-    (is (= [true 0 :anint] (f (parse-str "<anint>0</anint>") predef-types nil nil)))
-    (is (= [true 0 :abyte] (f (parse-str "<abyte>0</abyte>") predef-types nil nil)))
-    (is (= [false 128 :abyte] (f (parse-str "<abyte>128</abyte>") predef-types nil nil)))
-    (is (= [true 36 :my] (f (parse-str "<my>36</my>") predef-types nil nil)))
-    (is (= [false 35 :my] (f (parse-str "<my>35</my>") predef-types nil nil)))
+    (is (= [true 0 :anint] (f (parse-str "<anint>0</anint>") predef-types {} {})))
+    (is (= [true 0 :abyte] (f (parse-str "<abyte>0</abyte>") predef-types {} {})))
+    (is (= [false 128 :abyte] (f (parse-str "<abyte>128</abyte>") predef-types {} {})))
+    (is (= [true 36 :my] (f (parse-str "<my>36</my>") predef-types {} {})))
+    (is (= [false 35 :my] (f (parse-str "<my>35</my>") predef-types {} {})))
   ))
 
 
@@ -131,9 +131,9 @@
              </simpleType>
            <element name=\"theunion\" type=\"myunion\"/>
            </schema>")]
-    (is (= [false 35 :theunion] (f (parse-str "<theunion>35</theunion>") predef-types nil nil)))
-    (is (= [true 36 :theunion] (f (parse-str "<theunion>36</theunion>") predef-types nil nil)))
-    (is (= [true "small" :theunion] (f (parse-str "<theunion>small</theunion>") predef-types nil nil)))
+    (is (= [false 35 :theunion] (f (parse-str "<theunion>35</theunion>") predef-types {} {})))
+    (is (= [true 36 :theunion] (f (parse-str "<theunion>36</theunion>") predef-types {} {})))
+    (is (= [true "small" :theunion] (f (parse-str "<theunion>small</theunion>") predef-types {} {})))
     ))
 
 
@@ -143,11 +143,11 @@
 		         <element name=\"seq1\" type=\"integer\"/>
 		         <element name=\"seq2\" type=\"string\"/>
 		       </sequence>")]
-    (is (= [true [[true 1 :seq1] [true "adsf" :seq2]]] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>adsf</seq2></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq2>2</seq2><seq1>asdf</seq1></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>2</seq2><seq3>2</seq3></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq3>1</seq3></udr>")) predef-types nil nil)))
+    (is (= [true [[true 1 :seq1] [true "adsf" :seq2]]] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>adsf</seq2></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq2>2</seq2><seq1>asdf</seq1></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>2</seq2><seq3>2</seq3></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq3>1</seq3></udr>")) predef-types {} {})))
     ))
 (deftest test-all 
   (let [f (validation-fn-of 
@@ -155,11 +155,11 @@
 		         <element name=\"seq1\" type=\"integer\"/>
 		         <element name=\"seq2\" type=\"string\"/>
 		       </all>")]
-    (is (= [true [[true 1 :seq1][true "asdf" :seq2]]] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>asdf</seq2></udr>")) predef-types nil nil)))
-    (is (= [true [[true "asdf" :seq2][true 10 :seq1]]] (f (:content (parse-str "<udr><seq2>asdf</seq2><seq1>10</seq1></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>2</seq2><seq3>2</seq3></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq3>1</seq3></udr>")) predef-types nil nil)))
+    (is (= [true [[true 1 :seq1][true "asdf" :seq2]]] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>asdf</seq2></udr>")) predef-types {} {})))
+    (is (= [true [[true "asdf" :seq2][true 10 :seq1]]] (f (:content (parse-str "<udr><seq2>asdf</seq2><seq1>10</seq1></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>2</seq2><seq3>2</seq3></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq3>1</seq3></udr>")) predef-types {} {})))
     ))
 (deftest test-choice 
   (let [f (validation-fn-of 
@@ -167,12 +167,12 @@
 		         <element name=\"seq1\" type=\"integer\"/>
 		         <element name=\"seq2\" type=\"string\"/>
 		       </choice>")]
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>2</seq2></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq2>2</seq2><seq1>1</seq1></udr>")) predef-types nil nil)))
-    (is (= [true [true 1 :seq1]] (f (:content (parse-str "<udr><seq1>1</seq1></udr>")) predef-types nil nil)))
-    (is (= [true [true "asdf" :seq2]] (f (:content (parse-str "<udr><seq2>asdf</seq2></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>2</seq2><seq3>2</seq3></udr>")) predef-types nil nil)))
-    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq3>1</seq3></udr>")) predef-types nil nil)))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>2</seq2></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq2>2</seq2><seq1>1</seq1></udr>")) predef-types {} {})))
+    (is (= [true [true 1 :seq1]] (f (:content (parse-str "<udr><seq1>1</seq1></udr>")) predef-types {} {})))
+    (is (= [true [true "asdf" :seq2]] (f (:content (parse-str "<udr><seq2>asdf</seq2></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq2>2</seq2><seq3>2</seq3></udr>")) predef-types {} {})))
+    (is (= [false []] (f (:content (parse-str "<udr><seq1>1</seq1><seq3>1</seq3></udr>")) predef-types {} {})))
     ))
 
 
@@ -203,10 +203,10 @@
 		<element name=\"udr\" type=\"cp\">
 		  </element>
     </schema>")]
-    (is (= [true [[false 0 :uniontest]] :udr] (f (parse-str "<udr><uniontest>0</uniontest></udr>") predef-types nil nil)))
-    (is (= [true [[true 36 :uniontest]] :udr] (f (parse-str "<udr><uniontest>36</uniontest></udr>") predef-types nil nil)))
-    (is (= [true [[true "small" :uniontest]] :udr] (f (parse-str "<udr><uniontest>small</uniontest></udr>") predef-types nil nil)))
-    (is (= [true [[false "randomstring" :uniontest]] :udr] (f (parse-str "<udr><uniontest>randomstring</uniontest></udr>") predef-types nil nil)))
+    (is (= [true [[false 0 :uniontest]] :udr] (f (parse-str "<udr><uniontest>0</uniontest></udr>") predef-types {} {})))
+    (is (= [true [[true 36 :uniontest]] :udr] (f (parse-str "<udr><uniontest>36</uniontest></udr>") predef-types {} {})))
+    (is (= [true [[true "small" :uniontest]] :udr] (f (parse-str "<udr><uniontest>small</uniontest></udr>") predef-types {} {})))
+    (is (= [true [[false "randomstring" :uniontest]] :udr] (f (parse-str "<udr><uniontest>randomstring</uniontest></udr>") predef-types {} {})))
     
     ))
     
@@ -279,7 +279,7 @@
 			      </sequence>
 			    </extension>
           </complexContent>")]
-    (is (= [:sequence [:firstname :lastname :address :city :country :zip]] (schema nil types nil nil)))
+    (is (= [:sequence [:firstname :lastname :address :city :country :zip]] (schema nil types {} {})))
     (is (= [true [[true "Broadway" :address] [true "New" :city] [true "USA" :country] [true 123 :zip] [true "Donald" :firstname] [true "Duck" :lastname]]] (schema (:content 
                  (parse-str  "<employee>
                               <address>Broadway</address>
@@ -288,7 +288,7 @@
                               <firstname>Donald</firstname>
                               <lastname>Duck</lastname>
                               <zip>123</zip>
-                            </employee>")) types nil nil)))
+                            </employee>")) types {} {})))
     
     (is (= [true [[true "Broadway" :address] [true "LA" :city] [true "USA" :country] [true 123 :zip] [true "Donald" :firstname] [true "Duck" :lastname]]] (schema (:content 
                  (parse-str  "<employee>
@@ -298,7 +298,7 @@
                               <firstname>Donald</firstname>
                               <lastname>Duck</lastname>
                               <zip>123</zip>
-                            </employee>")) types nil nil)))
+                            </employee>")) types {} {})))
     
     (is (= [false [[true "Donald" :firstname] [true "Duck" :lastname]]] (schema (:content 
                    (parse-str  "<employee>
@@ -307,7 +307,7 @@
                               <country>USA</country>
                               <firstname>Donald</firstname>
                               <lastname>Duck</lastname>
-                            </employee>")) types nil nil)))
+                            </employee>")) types {} {})))
     
    (is (= [true [[true "Broadway" :address] [true "LA" :city] [true "USA" :country] [false 128 :zip] [true "Donald" :firstname] [true "Duck" :lastname]]] (schema (:content 
                  (parse-str  "<employee>
@@ -317,7 +317,7 @@
                               <firstname>Donald</firstname>
                               <lastname>Duck</lastname>
                               <zip>128</zip>
-                            </employee>")) types nil nil)))
+                            </employee>")) types {} {})))
   ))
 
 
